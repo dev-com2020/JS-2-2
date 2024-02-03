93aae48089841862fcfc79a5ceac02996d501155
DEV-COM <kwp@tlen.pl>
2024-02-03 12:52:25 +0100

    12:52

--- notes/app.js
+++ notes/app.js
@@ -2,9 +2,13 @@ console.log('Start aplikacji');
 
 const fs = require('fs');
 const os = require('os');
+const notes = require('./notes')
 
 var user = os.userInfo();
 // fs.appendFileSync('user1.txt','Witaj ' + user.username + "!");
 fs.appendFileSync('user2.txt',`Witaj ${user.username} !`);
+
+notes.addNote();
+notes.add(4,4);
 
 console.log(user.username);
--- notes/notes.js
+++ notes/notes.js
@@ -2,9 +2,9 @@ console.log('Startuje z notes.js');
 
 module.exports.addNote = () => {
     console.log('addNote');
-    return 'New note';
+    return console.log('New note');
 };
 
 module.exports.add = (a,b) => {
-    return a + b;
+    return console.log(a + b);
 };
