e3fb48e1f1a50ff64125c72e75f6110e330d4dba
DEV-COM <kwp@tlen.pl>
2024-02-03 14:38:35 +0100

    14:38

--- notes/app.js
+++ notes/app.js
@@ -3,12 +3,7 @@ console.log('Start aplikacji');
 const fs = require('fs');
 const os = require('os');
 const notes = require('./notes');
-const _ = require('lodash');
 
-var user = os.userInfo();
-console.log(_.isString('Tomek'));
-var filtered = _.uniq(['Tomek',1,1,2,3,'Adam']);
-console.log(filtered);
 console.log(process.argv);
 var command = process.argv[2];
 console.log('Command:', command);
@@ -20,10 +15,3 @@ if (command === 'add') {
 } else {
     console.log('Komenda nie jest rozpoznana');
 }
-// fs.appendFileSync('user1.txt','Witaj ' + user.username + "!");
-// fs.appendFileSync('user2.txt',`Witaj ${user.username}, masz ${notes.age} !`);
-
-// notes.addNote();
-// notes.add(4,4);
-
-// console.log(user.username);
